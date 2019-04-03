2b9a8ddd203f5f83d109505904ece4caf5261995
Minyus <yusuke.minami86@gmail.com>
2019-04-03 21:31:09 +0800

    Added import pandas as pd

--- causallift/utils.py
+++ causallift/utils.py
@@ -1,6 +1,7 @@
 """ utils.py """
 """ Utility functions """
 
+import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.metrics import confusion_matrix
 
