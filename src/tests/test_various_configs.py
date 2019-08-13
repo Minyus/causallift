4d68dfdc73e799825328ba954ede6596d66d16c0
Minyus <yusuke.minami86@gmail.com>
2019-08-14 00:11:24 +0800

    Black

--- src/tests/test_various_configs.py
+++ src/tests/test_various_configs.py
@@ -200,9 +200,9 @@ def test_enable_ipw_without_known_propensity_no_runner():
 
 # def test_enable_ipw_without_known_propensity_parallel_runner():
 #
+#     seed = 0
 #
-#
-#     df = generate_data( \
+#     df = generate_data(
 #         N=1000,
 #         n_features=3,
 #         beta=[0, -2, 3, -5],  # Effect of [intercept and features] on outcome
@@ -212,12 +212,20 @@ def test_enable_ipw_without_known_propensity_no_runner():
 #         discrete_outcome=True,
 #         seed=seed,
 #         feature_effect=0,  # Effect of beta on treated outxome
-#         propensity_coef=[0, -1, 1, -1],  # Effect of [intercept and features] on propensity log-odds for treatment
-#         index_name='index')
+#         propensity_coef=[
+#             0,
+#             -1,
+#             1,
+#             -1,
+#         ],  # Effect of [intercept and features] on propensity log-odds for treatment
+#         index_name="index",
+#     )
 #
-#     train_df, test_df = train_test_split(df, test_size=0.2, stratify=df['Treatment'])
+#     train_df, test_df = train_test_split(df, test_size=0.2, stratify=df["Treatment"])
 #
-#     cl = CausalLift(train_df, test_df, enable_ipw=True, verbose=3, runner='ParallelRunner')
+#     cl = CausalLift(
+#         train_df, test_df, enable_ipw=True, verbose=3, runner="ParallelRunner"
+#     )
 #     train_df, test_df = cl.estimate_cate_by_2_models()
 #     estimated_effect_df = cl.estimate_recommendation_impact()
 #     assert isinstance(train_df, pd.DataFrame)
@@ -231,3 +239,4 @@ if __name__ == "__main__":
     test_disable_ipw()
     test_enable_ipw_with_known_propensity()
     test_enable_ipw_without_known_propensity_no_runner()
+    # test_enable_ipw_without_known_propensity_parallel_runner()
